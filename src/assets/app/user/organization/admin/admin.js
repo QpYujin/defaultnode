0b476649bc71dbbef9c66540e5a604110c46670a
EC2 Default User <ec2-user@ip-172-31-64-35.ec2.internal>
2017-09-14 18:25:25 +0000

    dev active for admin module

---
+++ src/assets/app/user/organization/admin/admin.js
@@ -0,0 +1,86 @@
+angular.module('app.user.organization.admin',[])
+  .config(['$stateProvider', '$locationProvider', '$urlRouterProvider',function ($stateProvider,$locationProvider,$urlRouterProvider){
+    $stateProvider.state('user.organization.admin', {
+      url: '/admin',
+      title: 'Admin',
+      views: {
+        'organization@user.organization': {
+          templateUrl: 'app/user/organization/admin/admin.tpl.jade',
+          controller: 'OrganizationProjectAdminCtrl'
+        }
+      }
+
+      /*resolve: {
+ *         projects: function (Restangular, $stateParams) {
+ *                   return Restangular
+ *                               .one('user',$stateParams.userId)
+ *                                           .one('organizations', $stateParams.organizationId)
+ *                                                       .one('admin').get()
+ *                                                               }
+ *                                                                     }*/
+    })
+
+   }])
+
+  .controller('OrganizationProjectAdminCtrl', function ($scope, $stateParams,StaticParams, breadcrumbItems, globalUtils,$stateParams,Restangular) {
+    console.log('Admin controller for client side cluster start');
+
+    $scope.startcluster = function () {
+    console.log("This is from the client side function for access",$scope.cluster.access);
+    console.log("This is from the client side function for secret",$scope.cluster.secret);    
+    Restangular
+            .one('organizations', String($stateParams.organizationId))
+            .one('admin')
+            .post('', $scope.cluster)
+            .then(function () {
+        swal('DONE', 'Cluster started', 'success');
+    })
+   }
+
+   $scope.destroycluster = function () {
+    console.log("This is from the client side function for access",$scope.cluster.access);
+    console.log("This is from the client side function for secret",$scope.cluster.secret);
+    Restangular
+            .one('organizations', String($stateParams.organizationId))
+            .one('admin')
+            .one('destroy')
+            .post('', $scope.cluster)
+            .then(function () {
+        swal('DONE', 'Cluster destroyed successfully', 'success');
+    })
+   }
+
+  $scope.getconfig = function () {
+    console.log("This is from the client side function for access",$scope.cluster.access);
+    console.log("This is from the client side function for secret",$scope.cluster.secret);
+    Restangular
+            .one('organizations', String($stateParams.organizationId))
+            .one('admin')
+            .one('getconfig')
+            .post('', $scope.cluster)
+            .then(function () {
+        swal('DONE', 'Created config successfully', 'success');
+    })   
+}
+
+ $scope.getstatus = function () {
+    console.log("This is from the client side function for access",$scope.cluster.access);
+    console.log("This is from the client side function for secret",$scope.cluster.secret);
+    Restangular
+            .one('organizations', String($stateParams.organizationId))
+            .one('admin')
+            .one('getstatus')
+            .post('', $scope.cluster)
+            .then(function () {
+        swal('DONE', 'kubctl node creation process have started, wait for while to bring up completly ', 'success');
+    })
+}
+
+
+
+
+
+
+
+
+})
